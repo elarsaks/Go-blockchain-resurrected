07e9d667c5ceceb05c80a3fc882697d8adeb29f8
Elar Saks <elarsaks@gmail.com>
2026-05-23 18:57:29 +0300

    add balance calculation tests

---
+++ pkg/block/balance_test.go
@@ -0,0 +1,41 @@
+package block
+
+import "testing"
+
+func TestCalculateTotalBalanceTracksReceivedAndSpentTransactions(t *testing.T) {
+	bc := NewBlockchain("miner", 5001)
+
+	bc.transactionPool = []*Transaction{
+		NewTransaction(MINING_SENDER, "alice", "initial balance", 10),
+	}
+	bc.CreateBlock(1, bc.LastBlock().Hash())
+
+	bc.transactionPool = []*Transaction{
+		NewTransaction("alice", "bob", "payment", 3),
+	}
+	bc.CreateBlock(2, bc.LastBlock().Hash())
+
+	aliceBalance, err := bc.CalculateTotalBalance("alice")
+	if err != nil {
+		t.Fatalf("CalculateTotalBalance(alice) returned error: %v", err)
+	}
+	if aliceBalance != 7 {
+		t.Fatalf("alice balance = %v, want 7", aliceBalance)
+	}
+
+	bobBalance, err := bc.CalculateTotalBalance("bob")
+	if err != nil {
+		t.Fatalf("CalculateTotalBalance(bob) returned error: %v", err)
+	}
+	if bobBalance != 3 {
+		t.Fatalf("bob balance = %v, want 3", bobBalance)
+	}
+}
+
+func TestCalculateTotalBalanceRejectsUnknownAddress(t *testing.T) {
+	bc := NewBlockchain("miner", 5001)
+
+	if balance, err := bc.CalculateTotalBalance("missing"); err == nil {
+		t.Fatalf("CalculateTotalBalance returned balance %v, want error", balance)
+	}
+}
